Fixed Excel Writing and updated ViewSim2D.py to read rotation data

ViewSimulation2D.py
@@ -27,6 +27,7 @@ import matplotlib.animation as animation
 #import pyvista as pv
 #from pyvistaqt import BackgroundPlotter
 import pandas as pd
+from pyoptics import dipoles
 
 def init():
     for trajectory in trajectories:
@@ -50,7 +51,14 @@ def update_scene():
     # We update the whole plotter
     p.update()
 
-
+def quaternion_to_rotation_matrix(q):
+    ### I LOVE THE INTERNET ###
+    w, x, y, z = q
+    return np.array([
+        [1 - 2*y**2 - 2*z**2, 2*x*y - 2*z*w, 2*x*z + 2*y*w],
+        [2*x*y + 2*z*w, 1 - 2*x**2 - 2*z**2, 2*y*z - 2*x*w],
+        [2*x*z - 2*y*w, 2*y*z + 2*x*w, 1 - 2*x**2 - 2*y**2]
+    ])
 
 ###################################################################################
 # Start of program
@@ -89,6 +97,7 @@ excel_output = project.output.excel_output
 hdf_output = project.output.hdf_output
 include_force = project.output.include_force
 include_couple = project.output.include_couple
+include_orientation = project.output.include_orientation
 #===========================================================================
 # Read beam options and create beam collection
 #===========================================================================
@@ -111,9 +120,10 @@ radius = radii[0] # because we cannot handle variable radii yet.
 density = particle_collection.get_particle_density()
 rho = density[0] # not yet implemented.
 positions = particle_collection.get_particle_positions()
+orientations = particle_collection.get_particle_orientations()
 
 for i in range(n_particles):
-    print(i,particle_types[i],ref_ind[i],colors[i],radii[i],density[i],positions[i])
+    print(i,particle_types[i],ref_ind[i],colors[i],radii[i],density[i],positions[i],orientations[i])
 
 #===============================================================
 # Read the excel file with spins
@@ -121,10 +131,14 @@ for i in range(n_particles):
 if hdf_output==True:
     h5_file = h5py.File(filename_hdf, 'r')
     print(h5_file.keys())
+    for name in h5_file:
+        print(name, h5_file[name].shape)
     particles = h5_file['positions'][()]
     #particles = h5_file['particles'][()]
+    allqs = h5_file['orientations'][()]
     h5_file.close()
     print(particles.shape)
+    print(allqs.shape)
     #print(positions.shape)
 
 elif excel_output==True:
@@ -133,8 +147,16 @@ elif excel_output==True:
     npdata = dfl.to_numpy()
     print(npdata.shape)
     particles = np.zeros((frames,n_particles,3),dtype=np.float64)
+    allqs = np.zeros((frames, n_particles, 4))
+    quat_offset = 1 + 3*n_particles
+    if include_force:
+        quat_offset += 3*n_particles
+    if include_couple:
+        quat_offset += 3*n_particles
     for i in range(n_particles):
         particles[:,i,0:3] = npdata[:,1+i*3:1+(i+1)*3]
+        start = quat_offset + i*4
+        allqs[:, i, :] = npdata[:, start:start+4]
 
 else:
     sys.exit("Unable to find input file:", filename_xl,"or",filename_hdf)
@@ -179,11 +201,75 @@ for i in range(frames):
         for k in range(3):
             positions[j][k][i] = particles[i,j,k]
 
+
+#### 
+
+
+
 ###################################################################################
 # Do the animation
 ###################################################################################
 
 
+primitive, number_of_dipoles = dipoles.sphere_positions(radius, dipole_radius)
+dipole_above_zero = []
+dipole_below_zero = []
+
+
+rotated_dipoles = primitive
+array_of_rotated_dipoles = np.tile(rotated_dipoles.astype(np.float64), (n_particles, 1, 1))
+
+list_of_ref_inds = []
+dipole_above_zero = []
+dipole_below_zero = []
+count = 0
+for tempparticle in range(n_particles):
+    templist = []
+    for dipole in rotated_dipoles:
+        if dipole[0] > 0:
+            templist.append(ref_ind[tempparticle][0])
+            dipole_above_zero.append(count)
+        else:
+            templist.append(ref_ind[tempparticle][1])
+            dipole_below_zero.append(count)
+        count += 1
+    temparray = np.array(templist)
+    list_of_ref_inds.append(temparray)
+array_of_ref_inds = np.array(list_of_ref_inds)
+
+### Apply initial quaternion rotation to dipoles
+for particle_iterator in range(n_particles):
+    init_rotation = quaternion_to_rotation_matrix(orientations[particle_iterator])
+    array_of_rotated_dipoles[particle_iterator] = array_of_rotated_dipoles[particle_iterator] @ init_rotation.T
+
+n_dipoles = len(primitive)
+
+dipole_positions = np.zeros((frames, n_particles*n_dipoles, 3))
+
+for f in range(frames):
+    count = 0
+    for p in range(n_particles):
+
+        q = allqs[f, p]
+        rot = quaternion_to_rotation_matrix(q)
+
+        rotated_dipoles = primitive @ rot.T
+
+        for d in rotated_dipoles:
+            dipole_positions[f, count] = particles[f, p] + d
+            count += 1
+
+
+
+
 fig,ax = project.display.plot_intensity(beam_collection)
 
-project.display.animate_particles(fig,ax,positions,radius,colors)
+
+# print(dipole_positions[0,])
+# project.display.animate_particles(fig,ax,positions,radius,colors)
+
+parpole_ani = project.display.plot_parpoles(fig,ax,positions,dipole_positions,radius,colors,dipole_above_zero,dipole_below_zero, allqs)
+ax.set_xlim(-2e-6,2E-6)
+ax.set_ylim(-2e-6,2E-6)
+parpole_ani.save(f"{filestem}_zoomed_time.mp4", dpi = 300, fps=60)
+plt.show()

pyoptics/beams.py
@@ -16,7 +16,10 @@ BEAMTYPE_GAUSS_BARTON5 = 1
 BEAMTYPE_GAUSS_CSP = 2
 BEAMTYPE_BESSEL = 3
 BEAMTYPE_LAGUERRE_GAUSSIAN = 4
-BEAMTYPE_SINGLE_MODE = 5
+#BEAMTYPE_SINGLE_MODE = 5
+BEAMTYPE_RICHARDS_WOLF = 5 # general purpose for LG and other beams
+BEAMTYPE_RICHARDS_WOLF_ANALYTIC = 6 # general purpose for LG and other beams
+BEAMTYPE_SINGLE_MODE = 7
 ###################################################################################
 # Jones vector names
 ###################################################################################
@@ -33,24 +36,18 @@ POLARISATION_iY = 7
 numpoints = 1
 
 # Find the library and load it
-#beams_path = ctypes.util.find_library(pyoptics.__path__[0]+"/beamslib")
-#if not beams_path:
-#    print("Unable to find the specified library ./beamslib.")
-#    sys.exit()
-#
-#try:
-#    Beams = ctypes.CDLL(beams_path)
-#except OSError:
-#    print("Unable to load the Beams C++ library")
-#    sys.exit()
-
 try:
     Beams = ctypes.cdll.LoadLibrary("./pyoptics/beamslib.dylib")
 except OSError:
     print("Unable to load the Beams C++ library")
     sys.exit()
 
-#
+# try:
+#     Beams = ctypes.CDLL(beams_path)
+# except OSError:
+#     print("Unable to load the Beams C++ library")
+#     sys.exit()
+# #
 # Beam class
 #
 class BEAM(ctypes.Structure):
@@ -68,7 +65,11 @@ class BEAM(ctypes.Structure):
         ('rotation', ctypes.c_double * 9),
         ('w0', ctypes.c_double),
         ('gouy', ctypes.c_int),
-        ('numkpoints', ctypes.c_int)
+        ('numkpoints', ctypes.c_int),
+        ('NA', ctypes.c_double),
+        ('nm', ctypes.c_double),
+        ('sigma', ctypes.c_double),
+        ('zernike', ctypes.c_double * 3)
     ]
 #
 # Beam collection class
@@ -195,7 +196,7 @@ def get_rotation_matrix(angle,zangle):
     return final_rotation.flatten()
     
     
-def make_beam(beam_type, E0, kk, kt_by_kz = 0.3, order = 0, gouy = 0, w0 = 1.2e-6, jones = None, rotation = None, translation = None, numkpoints = 20):
+def make_beam(beam_type, E0, kk, kt_by_kz = 0.3, order = 0, gouy = 0, w0 = 1.2e-6, jones = None, rotation = None, translation = None, numkpoints = 20, NA = 0.8, nm = 1.333, sigma=1.0, zernike = None):
     """
     Function to build a beam and return the structure.
     """
@@ -212,6 +213,9 @@ def make_beam(beam_type, E0, kk, kt_by_kz = 0.3, order = 0, gouy = 0, w0 = 1.2e-
     mybeam.w0 = w0
     mybeam.gouy = gouy
     mybeam.numkpoints = numkpoints # should be even
+    mybeam.NA = NA
+    mybeam.nm = nm
+    mybeam.sigma = sigma
 #
 # Build a Jones matrix
 #
@@ -237,6 +241,14 @@ def make_beam(beam_type, E0, kk, kt_by_kz = 0.3, order = 0, gouy = 0, w0 = 1.2e-
         mybeam.translation = np.ctypeslib.as_ctypes(beamposition)
     else:
         mybeam.translation = np.ctypeslib.as_ctypes(translation)
+#
+# zernike coefficients
+#
+    if zernike is None:
+        coeffs = np.array((0.0,0.0,0.0),dtype=np.float64) # specify position in metres
+        mybeam.zernike = np.ctypeslib.as_ctypes(coeffs)
+    else:
+        mybeam.zernike = np.ctypeslib.as_ctypes(zernike)
 
     return mybeam
     
@@ -257,7 +269,7 @@ def create_beam_collection(beaminfo,wavelength,nm=1.333):
         replaced by defaults.
     output: beam_collection (Ctypes struct array): an array of beams.
     """
-    BeamTypes = {"BEAMTYPE_PLANE": 0, "BEAMTYPE_GAUSS_BARTON5": 1, "BEAMTYPE_GAUSS_CSP": 2, "BEAMTYPE_BESSEL": 3, "BEAMTYPE_LAGUERRE_GAUSSIAN": 4, "BEAMTYPE_SINGLE_MODE": 5}
+    BeamTypes = {"BEAMTYPE_PLANE": 0, "BEAMTYPE_GAUSS_BARTON5": 1, "BEAMTYPE_GAUSS_CSP": 2, "BEAMTYPE_BESSEL": 3, "BEAMTYPE_LAGUERRE_GAUSSIAN": 4, "BEAMTYPE_RICHARDS_WOLF": 5, "BEAMTYPE_RICHARDS_WOLF_ANALYTIC": 6, "BEAMTYPE_SINGLE_MODE": 7}
     JonesTypes = {"POLARISATION_X":0, "POLARISATION_Y":1, "X":0, "Y":1, "x":0, "y":1, "POLARISATION_RCP":2, "POLARISATION_LCP":3, "iRCP":4,"POLARISATION_iRCP":4, "POLARISATION_iLCP":5, "iLCP":5, "HORIZONTAL": 0, "HOR": 0, "H": 0, "h": 0, "VERTICAL": 1, "VERT": 1, "V": 1, "v": 1, "RCP": 2, "rcp": 2, "LCP": 3, "lcp": 3, "POLARISATION_iX":6, "POLARISATION_iY":7}
 
     n_beams = len(beaminfo)
@@ -284,9 +296,13 @@ def create_beam_collection(beaminfo,wavelength,nm=1.333):
         beamposition = getbeamoption(beam,'translation')
         #
         numkpoints = int(getbeamoption(beam,'numkpoints'))
+        NA = float(getbeamoption(beam,'NA'))
+        nm = float(getbeamoption(beam,'nm'))
+        sigma = float(getbeamoption(beam,'sigma'))
+        zernike = getbeamoption(beam,'zernike')
 
         #beam_collection[i] = make_beam(beamtype, E0, kk, kt_by_kz=kt_by_kz, order=order, gouy=gouy, w0=w0, jones=jones_matrix, rotation=rotation_matrix, translation=beamposition, numkpoints=numkpoints)
-        next_beam = make_beam(beamtype, E0, kk, kt_by_kz=kt_by_kz, order=order, gouy=gouy, w0=w0, jones=jones_matrix, rotation=rotation_matrix, translation=beamposition, numkpoints=numkpoints)
+        next_beam = make_beam(beamtype, E0, kk, kt_by_kz=kt_by_kz, order=order, gouy=gouy, w0=w0, jones=jones_matrix, rotation=rotation_matrix, translation=beamposition, numkpoints=numkpoints, NA=NA, nm=nm, sigma=sigma, zernike=zernike)
         beam_collection.append(next_beam)
         i+=1
     collection = BEAM_COLLECTION(beam_collection)
@@ -309,6 +325,10 @@ def getbeamoption(beam,option):
                 "rotation":'0.0 0.0',
                 "translation":'0.0 0.0 0.0',
                 "numkpoints":20,
+                "NA":0.8,
+                "nm":1.333,
+                "sigma":1.0,
+                "zernike":'0.0 0.0 0.0'
                 }
 
     if option=="rotation":
@@ -333,6 +353,17 @@ def getbeamoption(beam,option):
             for j in range(min(len(fields),3)):
                 beamposition[j] = float(fields[j])
         return beamposition
+#
+    elif option=="zernike":
+        zernike = beam.get(option,Defaults[option][0])
+        fields = zernike.split(" ")
+        if fields[0]=="None":
+            zernike = np.array((0.0,0.0,0.0),dtype=np.float64)
+        else:
+            zernike = np.array((0.0,0.0,0.0),dtype=np.float64)
+            for j in range(min(len(fields),3)):
+                zernike[j] = float(fields[j])
+        return zernike
 #
     else:
         strvalue = beam.get(option,Defaults[option])

pyoptics/forces.py
@@ -28,14 +28,14 @@ def non_bonded(constant1, constant2, r):
     force = - constant1 * (1.0-r_abs/constant2)*r/r_abs
     return force
 
-def non_bonded_array(number_of_particles, displacements_matrix, positions, exceptions):
+def non_bonded_array(number_of_particles, displacements_matrix, positions, exceptions, sphere_radius):
     #
     # This version using a DPD-like non-bonded force expression.
     #
     displacements_matrix_T = np.transpose(displacements_matrix)
     #
     ConstantA = 1e-12 # 1pN reasonable max force
-    ConstantB = 0.6e-6 # guess at nearest separation
+    ConstantB = 2.5 * sphere_radius # guess at nearest separation
 #    nonbonded_force_matrix = np.zeros(
 #        [number_of_particles, number_of_particles], dtype=object
 #    )

visualise-particle.py
@@ -155,6 +155,10 @@ def perform_simulation(number_of_particles, positions, sphere_radius, dipole_rad
     #=========================================================
     # Main simulation loop
     #=========================================================
+    optical = np.zeros((3,3))
+    torques = np.zeros((3,3))
+    couples = np.zeros((3,3))
+    dipole_positions = np.zeros(((number_of_dipoles),3))
     for i in range(number_of_timesteps):
         #
         # Pass in list of dipole positions to generate total dipole array;
@@ -194,7 +198,7 @@ def perform_simulation(number_of_particles, positions, sphere_radius, dipole_rad
         #print("rotated_dipoles shape")
         #print(rotated_dipoles.shape)
         #print(f"BEFORE FRED {dipole_above_zero}")
-        optical, torques, couples, dipole_positions = dipoles.py_optical_force_torque_array(position_vectors, dipole_radius, array_of_rotated_dipoles, array_of_ref_inds, beam_collection)   
+        # optical, torques, couples, dipole_positions = dipoles.py_optical_force_torque_array(position_vectors, dipole_radius, array_of_rotated_dipoles, array_of_ref_inds, beam_collection)   
              
         #couples = None
         #include_couple==False